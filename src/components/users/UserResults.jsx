da64b1710ce35b0f599c9e323783b1febf3af671
MdialloC19 <mdiallo3211@gmail.com>
2024-02-24 23:58:48 +0000

    add userResults component

---
+++ src/components/users/UserResults.jsx
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+
+function UserResults() {
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
+      headers: {
+        Autorisation: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+      },
+    });
+
+    const data = await response.json();
+    console.log(data);
+  };
+
+  return <div></div>;
+}
+
+export default UserResults;
