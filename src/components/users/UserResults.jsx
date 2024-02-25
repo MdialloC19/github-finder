a81c46ff547fe5e55d8ed16bf487432a0d68d242
MdialloC19 <mdiallo3211@gmail.com>
2024-02-25 23:55:44 +0000

    add loading and displaying user

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,6 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Spinner from "../shared/Spinner";
+import UserItem from "./UserItem";
 
 function UserResults() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     fetchUsers();
   }, []);
@@ -8,15 +13,25 @@ function UserResults() {
   const fetchUsers = async () => {
     const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
       headers: {
-        Autorisation: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
       },
     });
 
     const data = await response.json();
-    console.log(data);
+    setUsers(data);
+    setLoading(false);
   };
-
-  return <div></div>;
+  if (!loading) {
+    return (
+      <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+        {users.map((user) => {
+          return <UserItem key={user.id} user={user} />;
+        })}
+      </div>
+    );
+  } else {
+    return <Spinner />;
+  }
 }
 
 export default UserResults;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./css/index.css";
+import "./css/spinner.css";
+
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
